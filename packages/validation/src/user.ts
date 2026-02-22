dca7e443d00a3a337013306083d0afc99f5a226f
Gautam Suthar <iamgautamsuthar@outlook.com>
2026-02-22 18:53:45 +0530

    add: basic user schema and types

---
+++ packages/validation/src/user.ts
@@ -0,0 +1,24 @@
+import z from "zod";
+
+export const userSchema = z.object({
+  name: z.string().min(3).max(20),
+  username: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(6).max(20),
+});
+export type User = z.infer<typeof userSchema>;
+
+
+export const createUserSchema = userSchema
+export type CreateUser = z.infer<typeof createUserSchema>;
+
+
+export const loginUserSchema = userSchema.pick({
+  email: true,
+  password: true,
+});
+export type LoginUser = z.infer<typeof loginUserSchema>;
+
+
+export const updateProfileSchema = userSchema.partial();
+export type UpdateProfile = z.infer<typeof updateProfileSchema>;
