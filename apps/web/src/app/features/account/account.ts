0728eb3f6f6fbae95451b6231f3d730b0461e8ae
Ishika Patel <ishikapatel1364@gmail.com>
2026-02-25 13:28:49 +0530

    feat: implement account management UI with user details and deletion option

--- apps/web/src/app/features/account/account.ts
+++ apps/web/src/app/features/account/account.ts
@@ -1,8 +1,19 @@
+import { AuthStore } from '@/app/store/auth';
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './account.html',
 })
-export class Account {}
+export class Account {
+  userEmail$!: Observable<string>;
+  constructor(private authStore: AuthStore) {
+    this.userEmail$ = this.authStore.user$.pipe(
+      filter((user): user is any => !!user),
+      map((user) => user.email),
+    );
+  }
+}
