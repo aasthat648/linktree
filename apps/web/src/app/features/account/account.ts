f422e6085a5f1b4b23d67570df7d7b8c723b89e0
Ishika Patel <ishikapatel1364@gmail.com>
2026-03-04 12:02:42 +0530

    feat: enhance account management with profile update and avatar upload functionality

--- apps/web/src/app/features/account/account.ts
+++ apps/web/src/app/features/account/account.ts
@@ -1,17 +1,30 @@
+import { ProfileService } from '@/app/core/services/profile-service';
 import { AuthStore } from '@/app/store/auth';
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UpdateProfileBody } from '@linktree/validation';
 import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-account',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, FormsModule],
   templateUrl: './account.html',
 })
 export class Account {
   userName$!: Observable<string>;
   Name$!: Observable<string>;
-  constructor(private authStore: AuthStore) {
+  avatarUrl: string = '';
+  name: string = '';
+  username: string = '';
+  bio: string = '';
+  imagePreview: string | ArrayBuffer | null = null;
+  selectedFile: File | null = null;
+
+  constructor(
+    private authStore: AuthStore,
+    private profileService: ProfileService,
+  ) {
     this.userName$ = this.authStore.user$.pipe(
       filter((user): user is any => !!user),
       map((user) => user.username),
@@ -20,5 +33,47 @@ export class Account {
       filter((user): user is any => !!user),
       map((user) => user.name),
     );
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+
+      const reader = new FileReader();
+      reader.onload = (e) => (this.avatarUrl = reader.result as string);
+      reader.readAsDataURL(file);
+
+      this.uploadFile(file);
+    }
+  }
+
+  async uploadFile(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res: any = await this.profileService.uploadAvatar(formData).toPromise();
+      this.avatarUrl = res.data.url;
+    } catch (err) {
+      console.error('Failed to upload avatar:', err);
+    }
+  }
+
+  saveProfile() {
+    const payload: UpdateProfileBody = {
+      display_name: this.name,
+      username: this.username,
+      bio: this.bio ?? '',
+      avatar_url: this.avatarUrl ?? '', // Send the relative URL
+    };
+
+    this.profileService.UpdateProfile(payload).subscribe({
+      next: (res) => {
+        console.log('Profile updated:', res.data);
+        // Optionally update AuthStore here
+      },
+      error: (err) => console.error('Update failed:', err),
+    });
   }
 }
